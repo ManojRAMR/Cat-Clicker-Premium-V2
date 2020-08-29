9ef40c64c6c61fbc2ea6b89afb94cf3fb9a1c315
Manoj R. Randeni <randeni.ramr@gmail.com>
2020-08-29 19:51:05 +0530

    update cat clicker js

--- app.js
+++ app.js
@@ -7,14 +7,81 @@
       { name: "Roy", img: "./cat4.webp", count: 0 },
       { name: "Tony", img: "./cat5.jpg", count: 0 },
     ],
-    currentCatID: null,
+    currentCat: null,
   };
 
-  var octopus = {};
+  var octopus = {
+    init: function () {
+      catListView.init();
+      this.setCurrentCat(data.cats[0]);
+      catView.init();
+    },
+    // Get cats from data
+    getCats: function () {
+      return data.cats;
+    },
 
-  var viewList = {};
+    setCurrentCat: function (cat) {
+      data.currentCat = cat;
+    },
 
-  var viewCat = {};
+    getCurrentCat: function () {
+      return data.currentCat;
+    },
+
+    incrmentCounter: function () {
+      data.currentCat.count++;
+      catView.render();
+    },
+  };
+
+  var catListView = {
+    // init cal list
+    init: function () {
+      this.catList = document.getElementById("catList");
+      this.render();
+    },
+
+    render: function () {
+      let cats = octopus.getCats();
+      cats.forEach((cat) => {
+        let elem = document.createElement("li");
+        elem.textContent = cat.name;
+        this.catList.appendChild(elem);
+
+        elem.addEventListener(
+          "click",
+          ((cat) => {
+            return () => {
+              octopus.setCurrentCat(cat);
+              catView.render();
+            };
+          })(cat)
+        );
+      });
+    },
+  };
+
+  var catView = {
+    init: function () {
+      this.catName = document.getElementById("catName");
+      this.clickCounter = document.getElementById("clickCounter");
+      this.catImg = document.getElementById("catImg");
+
+      this.catImg.addEventListener("click", function () {
+        octopus.incrmentCounter();
+      });
+
+      this.render();
+    },
+
+    render: function () {
+      let currentCat = octopus.getCurrentCat();
+      this.catName.textContent = currentCat.name;
+      this.catImg.src = currentCat.img;
+      this.clickCounter.textContent = currentCat.count;
+    },
+  };
 
   octopus.init();
 })();
