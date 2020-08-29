762357643033725a5a94ac9e8e43b9d5bfca0bf8
Manoj R. Randeni <randeni.ramr@gmail.com>
2020-08-29 18:52:57 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+  var data = {
+    cats: [
+      { name: "Kitty", img: "./cat1.webp", count: 0 },
+      { name: "Nicky", img: "./cat2.jpg", count: 0 },
+      { name: "Milly", img: "./cat3.jpg", count: 0 },
+      { name: "Roy", img: "./cat4.webp", count: 0 },
+      { name: "Tony", img: "./cat5.jpg", count: 0 },
+    ],
+    currentCatID: null,
+  };
+
+  var octopus = {};
+
+  var viewList = {};
+
+  var viewCat = {};
+
+  octopus.init();
+})();
