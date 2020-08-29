ee72bbfd8ba3c075c377ce0a417d54591487f398
Manoj R. Randeni <randeni.ramr@gmail.com>
2020-08-30 01:34:19 +0530

    Add admin form feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 (function () {
+  // Define cat data
   var data = {
     cats: [
       { name: "Kitty", img: "./cat1.webp", count: 0 },
@@ -8,31 +9,53 @@
       { name: "Tony", img: "./cat5.jpg", count: 0 },
     ],
     currentCat: null,
+    currentCatID: null,
   };
 
   var octopus = {
+    // Init app
     init: function () {
-      this.setCurrentCat(data.cats[0]);
+      this.setCurrentCat(data.cats[0], 0);
 
       catListView.init();
       catView.init();
+      adminForm.init();
     },
-    // Get cats from data
+
+    // Get all cats from data
     getCats: function () {
       return data.cats;
     },
 
-    setCurrentCat: function (cat) {
+    // Set current selected cat data
+    setCurrentCat: function (cat, id) {
       data.currentCat = cat;
+      data.currentCatID = id;
     },
 
+    // Get current selected cat data
     getCurrentCat: function () {
       return data.currentCat;
     },
 
+    // Get current selected cat data ID
+    getCurrentCatID: function () {
+      return data.currentCatID;
+    },
+
+    // Update cat data from admin form submits
+    updateCatsData: function (newData) {
+      data.cats[data.currentCatID] = newData;
+      this.setCurrentCat(newData, data.currentCatID);
+      catListView.render();
+      catView.render();
+    },
+
+    // Selected cat image click count incrementer
     incrmentCounter: function () {
       data.currentCat.count++;
       catView.render();
+      adminForm.render();
     },
   };
 
@@ -46,20 +69,23 @@
     // Render cat list
     render: function () {
       let cats = octopus.getCats();
-      cats.forEach((cat) => {
+      this.catList.innerHTML = "";
+      cats.forEach((cat, id) => {
         let elem = document.createElement("li");
         elem.textContent = cat.name;
         elem.className = "list-group-item list-group-item-action ";
 
+        // Event listener to cat name list to check selected cat
         elem.addEventListener(
           "click",
-          ((cat) => {
+          ((cat, id) => {
             return () => {
-              octopus.setCurrentCat(cat);
+              octopus.setCurrentCat(cat, id);
 
               catView.render();
+              adminForm.render();
             };
-          })(cat)
+          })(cat, id)
         );
         this.catList.appendChild(elem);
       });
@@ -68,11 +94,13 @@
 
   // Cat view object
   var catView = {
+    // Init cat view
     init: function () {
       this.catName = document.getElementById("catName");
       this.clickCounter = document.getElementById("clickCounter");
       this.catImg = document.getElementById("catImg");
 
+      // Cat image click event listener
       this.catImg.addEventListener("click", function () {
         octopus.incrmentCounter();
       });
@@ -80,11 +108,59 @@
       this.render();
     },
 
+    // Render selected cat view to screen
     render: function () {
       let currentCat = octopus.getCurrentCat();
       this.catName.textContent = currentCat.name;
       this.catImg.src = currentCat.img;
       this.clickCounter.textContent = currentCat.count;
+    },
+  };
+
+  var adminForm = {
+    // Init admin form
+    init: function () {
+      this.adminBtn = document.getElementById("adminBtn");
+      this.adminForm = document.getElementById("adminForm");
+      this.catNameInput = document.getElementById("catNameInput");
+      this.catImgUrlInput = document.getElementById("catImgUrlInput");
+      this.imgClickCount = document.getElementById("imgClickCount");
+      this.formCancel = document.getElementById("formCancel");
+      this.formSubmit = document.getElementById("formSubmit");
+
+      // Admin form admin btn event listener
+      this.adminBtn.addEventListener("click", () => {
+        this.adminForm.classList.toggle("d-none");
+      });
+
+      // Admin form cancel btn event listener
+      this.formCancel.addEventListener("click", () => {
+        this.adminForm.classList.toggle("d-none");
+      });
+
+      // Admin form submit btn event listener
+      this.formSubmit.addEventListener("click", () => {
+        this.submit();
+      });
+
+      this.render();
+    },
+
+    // Render Data to admin form
+    render: function () {
+      let currentCat = octopus.getCurrentCat();
+      this.catNameInput.value = currentCat.name;
+      this.catImgUrlInput.value = currentCat.img;
+      this.imgClickCount.value = currentCat.count;
+    },
+
+    // Submit user admin form data to Data
+    submit: function () {
+      octopus.updateCatsData({
+        name: this.catNameInput.value,
+        img: this.catImgUrlInput.value,
+        count: this.imgClickCount.value,
+      });
     },
   };
 
